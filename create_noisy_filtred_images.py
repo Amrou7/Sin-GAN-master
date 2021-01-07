050293db433a1114fd8837baf2df29b6ebf7dcc1
WassimBouatay <54542480+WassimBouatay@users.noreply.github.com>
2021-01-07 18:50:59 +0100

    Update create_noisy_filtred_images.py

--- create_noisy_filtred_images.py
+++ create_noisy_filtred_images.py
@@ -1,36 +1,39 @@
 import numpy as np
-import matplotlib
 import matplotlib.pylab as plt
 from scipy.ndimage import gaussian_filter
 from skimage.transform import resize
 from scipy import ndimage
 import imageio
 
+def psnr(clean, img):
+    img = img
+    clean = clean
+    mse = np.mean((clean-img)**2)
+    if mse == 0:
+        return 100
+    PIXEL_MAX = 1
+    return 20 * np.log10(PIXEL_MAX/np.sqrt(mse))
+
 
 input_path = 'C:/Files/M2 MVA/S1/Object recognition/Project/SinGAN-master/Input/Set14/'
 noisy_path = 'C:/Files/M2 MVA/S1/Object recognition/Project/SinGAN-master/Input/GaussianNoise/'
-filtered_path = 'C:/Files/M2 MVA/S1/Object recognition/Project/SinGAN-master/Input/Filtered/'
+output_path = 'C:/Files/M2 MVA/S1/Object recognition/Project/SinGAN-master/Output/Paint2image/'
 
-image_name = 'baboon.png'
-image = imageio.imread(input_path+image_name)/255
-new_shape = (image.shape[0]/2, image.shape[1]/2, image.shape[2])
-image = resize(image, new_shape, mode='reflect')
+image = imageio.imread(input_path+"Lenna.png")/255
+noisy = imageio.imread(noisy_path+'sigma=30-Lenna.png')/255
+noisy = resize(noisy, image.shape, mode='reflect')
+plt.imshow(noisy)
+plt.show()
 plt.imshow(image)
 plt.show()
 
-noise = 0.2
-noisy_image = image + np.random.normal(0, noise, image.shape)
-noisy_image[noisy_image>1] = 1 
-noisy_image[noisy_image<0] = 0 
-plt.imshow(noisy_image)
+output = imageio.imread(output_path+'sigma=30-Lenna/sigma=30-Lenna_out/start_scale=9.png')/255
+output = resize(output, image.shape, mode='reflect')
+plt.imshow(output)
 plt.show()
-imageio.imwrite(noisy_path+image_name, noisy_image)
 
 
-filtered = np.zeros(image.shape)
-filtered[:,:,0] = ndimage.median_filter(noisy_image[:,:,0], 7)
-filtered[:,:,1] = ndimage.median_filter(noisy_image[:,:,1], 7)
-filtered[:,:,2] = ndimage.median_filter(noisy_image[:,:,2], 7)
-plt.imshow(filtered)
-plt.show()
-imageio.imwrite(filtered_path+image_name, filtered)
+print("Noisy image PSNR", psnr(image, noisy))
+print("Denoised image PSNR", psnr(image, output))
+
+
