063f65f38e5fd756f1425afafaba0db1578b4f7e
WassimBouatay <54542480+WassimBouatay@users.noreply.github.com>
2021-01-08 19:16:36 +0100

    Update PSRN.py

--- PSRN.py
+++ PSRN.py
@@ -18,22 +18,27 @@ def psnr(clean, img):
 input_path = 'C:/Files/M2 MVA/S1/Object recognition/Project/SinGAN-master/Input/Set14/'
 noisy_path = 'C:/Files/M2 MVA/S1/Object recognition/Project/SinGAN-master/Input/GaussianNoise/'
 output_path = 'C:/Files/M2 MVA/S1/Object recognition/Project/SinGAN-master/Output/Paint2image/'
+filtered_path = 'C:/Files/M2 MVA/S1/Object recognition/Project/SinGAN-master/Input/Filtered/'
 
-image = imageio.imread(input_path+"Lenna.png")/255
-noisy = imageio.imread(noisy_path+'sigma=30-Lenna.png')/255
+image = imageio.imread(input_path+"pepper.png")/255
+noisy = imageio.imread(noisy_path+'sigma=30-pepper.png')/255
 noisy = resize(noisy, image.shape, mode='reflect')
 plt.imshow(noisy)
 plt.show()
 plt.imshow(image)
 plt.show()
 
-output = imageio.imread(output_path+'sigma=30-Lenna/sigma=30-Lenna_out/start_scale=9.png')/255
+
+output = imageio.imread(output_path+'f-sigma=30-pepper/sigma=30-pepper_out/start_scale=7.png')/255
 output = resize(output, image.shape, mode='reflect')
 plt.imshow(output)
 plt.show()
-
-
+    
 print("Noisy image PSNR", psnr(image, noisy))
 print("Denoised image PSNR", psnr(image, output))
 
-
+filtered = imageio.imread(filtered_path+"f-sigma=30-pepper.png")/255
+filtered = resize(filtered, image.shape, mode='reflect')
+print("Filtered image PSNR", psnr(image, filtered))
+plt.imshow(filtered)
+plt.show()
