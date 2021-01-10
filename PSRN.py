696a47dc55570099be0b631500727d03244bab7f
WassimBouatay <54542480+WassimBouatay@users.noreply.github.com>
2021-01-10 18:42:34 +0100

    Update PSRN.py

--- PSRN.py
+++ PSRN.py
@@ -19,26 +19,42 @@ input_path = 'C:/Files/M2 MVA/S1/Object recognition/Project/SinGAN-master/Input/
 noisy_path = 'C:/Files/M2 MVA/S1/Object recognition/Project/SinGAN-master/Input/GaussianNoise/'
 output_path = 'C:/Files/M2 MVA/S1/Object recognition/Project/SinGAN-master/Output/Paint2image/'
 filtered_path = 'C:/Files/M2 MVA/S1/Object recognition/Project/SinGAN-master/Input/Filtered/'
+NLmeans_path = 'C:/Files/M2 MVA/S1/Object recognition/Project/SinGAN-master/Input/NLmeans/Gaussian/'
 
-image = imageio.imread(input_path+"pepper.png")/255
-noisy = imageio.imread(noisy_path+'sigma=30-pepper.png')/255
-noisy = resize(noisy, image.shape, mode='reflect')
-plt.imshow(noisy)
-plt.show()
+
+#### Clean image
+image = imageio.imread(input_path+"bridge.png")/255
 plt.imshow(image)
 plt.show()
 
+#### Noisy image
+noisy = imageio.imread(noisy_path+'sigma=30-bridge.png')/255
+noisy = resize(noisy, image.shape, mode='reflect')
+plt.imshow(noisy)
+plt.show()
+print("Noisy image PSNR", psnr(image, noisy))
 
-output = imageio.imread(output_path+'f-sigma=30-pepper/sigma=30-pepper_out/start_scale=7.png')/255
+
+#### SinGAN result
+#output = imageio.imread(output_path+'NL-sigma=30-lenna/sigma=30-lenna_out/start_scale=7.png')/255
+output = imageio.imread(output_path+'f-sigma=30-bridge/start_scale=8.png')/255
 output = resize(output, image.shape, mode='reflect')
 plt.imshow(output)
 plt.show()
-    
-print("Noisy image PSNR", psnr(image, noisy))
-print("Denoised image PSNR", psnr(image, output))
+print("sinGAN Denoised image PSNR", psnr(image, output))
 
-filtered = imageio.imread(filtered_path+"f-sigma=30-pepper.png")/255
+
+### Median filter
+filtered = imageio.imread(filtered_path+"f-sigma=30-bridge.png")/255
 filtered = resize(filtered, image.shape, mode='reflect')
-print("Filtered image PSNR", psnr(image, filtered))
+print("Median-Filter denoised image PSNR", psnr(image, filtered))
 plt.imshow(filtered)
+plt.show()
+
+
+#### NLmeans
+NLmeans_denoised = imageio.imread(NLmeans_path+"NL-sigma=30-barbara.png")/255
+NLmeans_denoised = resize(NLmeans_denoised, image.shape, mode='reflect')
+print("NLmeans denoised image PSNR", psnr(image, NLmeans_denoised))
+plt.imshow(NLmeans_denoised)
 plt.show()
